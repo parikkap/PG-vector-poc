141bfef4b1d35b8b9bc36cc161d54f892a6dbd5d
parikkap <peter.parikka@loihdefactor.com>
2023-10-20 16:23:16 +0300

    Send promts trough gpt3.5

--- src/app/server/router/embeddingRouter.ts
+++ src/app/server/router/embeddingRouter.ts
@@ -5,18 +5,18 @@ import { openAiClient } from "../../lib/openAIClient";
 import { z } from "zod";
 
 import { publicProcedure, router } from "../trpc";
-import { Prisma } from "@prisma/client";
+import { stripIndent, oneLine } from "common-tags";
 
 export const embeddingRouter = router({
   addEmbedding: publicProcedure
     .input(z.string())
     .mutation(async ({ input: prompt }) => {
-      const processedText = prompt.replace(/\n/g, " ");
+      const processedPrompt = prompt.replace(/\n/g, " ");
 
       try {
         const embedding = await openAiClient.embeddings.create({
           model: "text-embedding-ada-002",
-          input: processedText,
+          input: processedPrompt,
         });
 
         const vector = embedding.data[0].embedding;
@@ -30,23 +30,37 @@ export const embeddingRouter = router({
         //   vectorSql
         // );
 
-        const items =
+        const items: { id: number; embedding: string; document: string }[] =
           await prisma.$queryRaw`SELECT id, embedding::text, document FROM item ORDER BY embedding <-> ${vectorSql}::vector LIMIT 5`;
 
-        console.log("items", items);
+        const chatGptPrompt = stripIndent`${oneLine`
+          You are a very enthusiastic Supabase representative who loves
+          to help people! Given the following sections from the Supabase
+          documentation, answer the question using only that information,
+          outputted in markdown format. If you are unsure and the answer
+          is not explicitly written in the documentation, say
+          "Sorry, I don't know how to help with that."`}
+      
+          Context sections:
+          ${items[0].document}
+      
+          Question: """
+          ${processedPrompt}
+          """
+      
+          Answer as markdown (including related code snippets if available):
+        `;
+        const response = await openAiClient.chat.completions.create({
+          messages: [{ content: chatGptPrompt, role: "user" }],
+          model: "gpt-3.5-turbo",
+          max_tokens: 512,
+          temperature: 0,
+        });
+        console.log("response", response);
+        console.log("message", response.choices[0].message);
       } catch (error) {
         console.error("Error:", error);
       }
-
-      //   const response = await openAiClient.chat.completions.create({
-      //     messages: [{ content: prompt, role: "user" }],
-      //     model: "gpt-3.5-turbo",
-      //   });
-
-      //   console.log("response", response);
-      //   console.log("response", response.choices[0].message);
-
-      //   SELECT * FROM items ORDER BY embedding <-> '[3,1,2]' LIMIT 5;
 
       try {
         // return response.choices[0].message;
@@ -59,9 +73,7 @@ export const embeddingRouter = router({
   addPdf: publicProcedure
     .input(z.string().nullable())
     .mutation(async ({ input: prompt }) => {
-      const text = `Cloud computing is the delivery of computing services over the internet. Computing services include common IT infrastructure such as virtual machines, storage, databases, and networking. Cloud services also expand the traditional IT offerings to include things like Internet of Things (IoT), machine learning (ML), and artificial intelligence (AI).
-
-Because cloud computing uses the internet to deliver these services, it doesn’t have to be constrained by physical infrastructure the same way that a traditional datacenter is. That means if you need to increase your IT infrastructure rapidly, you don’t have to wait to build a new datacenter—you can use the cloud to rapidly expand your IT footprint.`;
+      const text = `Cloud computing is the delivery of computing services over the internet. Computing services include common IT infrastructure such as virtual machines, storage, databases, and networking. Cloud services also expand the traditional IT offerings to include things like Internet of Things (IoT), machine learning (ML), and artificial intelligence (AI). Because cloud computing uses the internet to deliver these services, it doesn’t have to be constrained by physical infrastructure the same way that a traditional datacenter is. That means if you need to increase your IT infrastructure rapidly, you don’t have to wait to build a new datacenter—you can use the cloud to rapidly expand your IT footprint.`;
 
       const text2 = `An on-premises data center is a group of servers that you privately own and control. Traditional cloud computing (as opposed to hybrid or private cloud computing models) involves leasing data center resources from a third-party service provider.`;
 
