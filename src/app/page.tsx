315e779d034c028843c9d45f893f434c42bd74f5
AleksiStatse <aleksi.statsevich@loihdefactor.com>
2023-10-24 13:22:45 +0300

    Configurate eslint and prettier. Create a vsc workspace.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,59 @@
-"use client";
-import React from "react";
-import { trpc } from "./_trpc/client";
-import Image from "next/image"
+'use client'
+import React from 'react'
+import { trpc } from './_trpc/client'
+import Image from 'next/image'
 
 type ChatMessageType = {
-  sender: "user" | "server"
+  sender: 'user' | 'server'
   message: string
 }
 
 export default function Home() {
   const [loading, setLoading] = React.useState<boolean>(false)
-  const [userMessage, setUserMessage] = React.useState<string>("")
+  const [userMessage, setUserMessage] = React.useState<string>('')
   const [chat, setChat] = React.useState<ChatMessageType[]>([
     {
-      sender: "server",
-      message: "Please ask a question"
-    }
-  ]) 
-  const containerRef = React.useRef<HTMLDivElement>(null);
-  const addTodo = trpc.todo.addEmbedding.useMutation();
-  const addPdf = trpc.todo.addPdf.useMutation();
+      sender: 'server',
+      message: 'Please ask a question',
+    },
+  ])
+  const containerRef = React.useRef<HTMLDivElement>(null)
+  const addTodo = trpc.todo.addEmbedding.useMutation()
+  const addPdf = trpc.todo.addPdf.useMutation()
 
   const scrollToBottom = () => {
     if (containerRef.current) {
-      containerRef.current.scrollTop = containerRef.current.scrollHeight;
+      containerRef.current.scrollTop = containerRef.current.scrollHeight
     }
-  };
+  }
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     scrollToBottom()
   }, [chat])
 
   const updateChat = (message: ChatMessageType) => {
-    setChat(prevChat => [...prevChat, message])
+    setChat((prevChat) => [...prevChat, message])
   }
 
   const sendMessageToServer = async () => {
     if (!userMessage || loading) {
-      return;
+      return
     }
     setLoading(true)
-    updateChat({sender: "user", message: userMessage})
-    setUserMessage("")
-    
-    const response = await addTodo.mutateAsync(userMessage);
+    updateChat({ sender: 'user', message: userMessage })
+    setUserMessage('')
+
+    const response = await addTodo.mutateAsync(userMessage)
     const serverMessage = response?.content
 
     if (!serverMessage) {
       setLoading(false)
-      throw new Error("Error: No message from server!")
+      throw new Error('Error: No message from server!')
     }
 
     updateChat({
-      sender: "server",
-      message: serverMessage
+      sender: 'server',
+      message: serverMessage,
     })
 
     setLoading(false)
@@ -64,20 +64,23 @@ export default function Home() {
       <div className="drawer drawer-open">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col items-center justify-center relative">
-            <div className="flex flex-col w-full gap-4 h-screen pt-8 pb-28 ">
+          <div className="flex flex-col w-full gap-4 h-screen pt-8 pb-28 ">
             <div id="chat" ref={containerRef} className="overflow-scroll px-8">
-              {chat.map((item, index)=><ChatComponent key={index} {...item}/>)}
-              {loading ?  
+              {chat.map((item, index) => (
+                <ChatComponent key={index} {...item} />
+              ))}
+              {loading ? (
                 <div className="chat chat-start">
                   <div className="chat-image avatar">
-                      <div className="w-10 rounded-full bg-red">
-                        <Image src="/next.svg" width={100} height={100} alt=""/>
-                      </div>
+                    <div className="w-10 rounded-full bg-red">
+                      <Image src="/next.svg" width={100} height={100} alt="" />
                     </div>
+                  </div>
                   <div className="chat-bubble chat-bubble-primary">
                     <span className="loading loading-dots loading-xs"></span>
                   </div>
-                </div> : null}
+                </div>
+              ) : null}
             </div>
             <div className="mt-auto w-full bg-base-100 bottom-0 left-0 py-4 absolute border-t-2">
               <div className="px-8 flex flex-row gap-2 ">
@@ -91,8 +94,7 @@ export default function Home() {
                     if (event.key === 'Enter') {
                       sendMessageToServer()
                     }
-                  }
-                }
+                  }}
                 />
                 <button
                   className="btn"
@@ -103,45 +105,57 @@ export default function Home() {
                 </button>
               </div>
             </div>
+          </div>
         </div>
-        </div> 
         <div className="drawer-side bg-base-200 p-4">
           <ul className="menu w-80 min-h-fulltext-base-content">
             {/* List uploaded files */}
             <li>File.pdf</li>
           </ul>
           <div className="flex flex-col gap-4">
-            <label htmlFor="my-drawer-2" aria-label="close sidebasr" className="drawer-overlay"></label> 
+            <label
+              htmlFor="my-drawer-2"
+              aria-label="close sidebasr"
+              className="drawer-overlay"
+            ></label>
             <input type="file" className="file-input w-full max-w-xs" />
             <button
               className="btn btn-primary"
               onClick={() => {
-                const response = addPdf.mutate(null);
+                addPdf.mutate(null)
               }}
             >
               Add pdf
-            </button> 
-          </div> 
+            </button>
+          </div>
         </div>
-      </div>  
-    
+      </div>
     </main>
-  );
+  )
 }
 
 const ChatComponent = (chat: ChatMessageType) => {
-  const isServer = chat.sender === "server"
+  const isServer = chat.sender === 'server'
   return (
-    <div className={`chat ${isServer ? "chat-start" : "chat-end"}`}>
-    <div className="chat-image avatar">
-      <div className="w-10 rounded-full bg-red">
-        <Image src={isServer ? "/next.svg" : "/vercel.svg"} width={100} height={100} alt=""/>
+    <div className={`chat ${isServer ? 'chat-start' : 'chat-end'}`}>
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full bg-red">
+          <Image
+            src={isServer ? '/next.svg' : '/vercel.svg'}
+            width={100}
+            height={100}
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="chat-header">{chat.sender}</div>
+      <div
+        className={`chat-bubble ${
+          isServer ? 'chat-bubble-primary' : 'chat-bubble-secondary'
+        } `}
+      >
+        {chat.message}
       </div>
     </div>
-    <div className="chat-header">
-      {chat.sender}
-    </div>
-    <div className={`chat-bubble ${isServer ? "chat-bubble-primary" : "chat-bubble-secondary"} `}>{chat.message}</div>
-  </div>
   )
 }
